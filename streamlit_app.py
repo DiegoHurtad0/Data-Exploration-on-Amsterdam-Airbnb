a2c92faa82417ab20b35f7f544ee65b2bedef20f
Diego Gustavo Hurtado Olivares <44350813+DiegoHurtad0@users.noreply.github.com>
2023-03-10 02:07:34 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,22 @@
 import pandas as pd
 import plotly.express as px
+# from PIL import Image
+
 import streamlit as st
+# from streamlit_lottie import st_lottie
+# from streamlit_lottie import st_lottie_spinner
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
 
 # Display title and text
-st.title("Amsterdam Airbnb")
+# Confit
+st.set_page_config(page_title='Amsterdam Airbnb Tool', page_icon=':bar_chart:', layout='wide')
+#st.title("Amsterdam Airbnb")
 st.markdown("Data Exploration on Amsterdam Airbnb")
 
 # Read dataframe
@@ -52,4 +65,26 @@ fig.update_geos(center=dict(lat=df.iloc[0][2], lon=df.iloc[0][3]))
 # Show the figure
 st.plotly_chart(fig, use_container_width=True)
 
-st.markdown("Diego Hurtado")
+st.markdown("Diego Gustavo Hurtado Olivares")
+
+# lottie_url_hello = "https://assets5.lottiefiles.com/packages/lf20_V9t630.json"
+# lottie_url_download = "https://assets4.lottiefiles.com/private_files/lf30_t26law.json"
+# lottie_hello = load_lottieurl(lottie_url_hello)
+# lottie_download = load_lottieurl(lottie_url_download)
+
+
+# st_lottie(lottie_hello, key="hello")
+
+# if st.button("Download"):
+#     with st_lottie_spinner(lottie_download, key="download"):
+#         time.sleep(5)
+#     st.balloons()
+
+
+c1, c2, c3 = st.columns(3)
+with c1:
+    st.info('**Data Scientist: [@Diego](https://www.linkedin.com/in/diegohurtadoo/)**', icon="💡")
+with c2:
+    st.info('**GitHub: [@DiegoHurtad0](https://github.com/DiegoHurtad0/DiegoHurtad0)**', icon="💻")
+with c3:
+    st.info('**Medium: [Diego Hurtado](https://medium.com/@diego.hurtado.olivares)**', icon="🧠")
