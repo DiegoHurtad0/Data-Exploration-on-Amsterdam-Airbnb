56491ddfb0859e3804be62b258fbeb66e2dbbf38
Diego Gustavo Hurtado Olivares <44350813+DiegoHurtad0@users.noreply.github.com>
2023-03-10 01:26:11 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,54 +3,52 @@ import plotly.express as px
 import streamlit as st
 
 # Display title and text
-st.title("Week 1 - Data and visualization")
-st.markdown("Here we can see the dataframe created during this weeks project.")
+st.title("Amsterdam Airbnb")
+st.markdown("Data Exploration on Amsterdam Airbnb")
 
 # Read dataframe
 dataframe = pd.read_csv(
     "data/WK1_Airbnb_Amsterdam_listings_proj_solution.csv",
-    names=[
-        "Airbnb Listing ID",
-        "Price",
-        "Latitude",
-        "Longitude",
-        "Meters from chosen location",
-        "Location",
-    ],
 )
 
 # We have a limited budget, therefore we would like to exclude
-# listings with a price above 100 pounds per night
-dataframe = dataframe[dataframe["Price"] <= 100]
-
-# Display as integer
-dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
-# Round of values
-dataframe["Price"] = "£ " + dataframe["Price"].round(2).astype(str) # <--- CHANGE THIS POUND SYMBOL IF YOU CHOSE CURRENCY OTHER THAN POUND
-# Rename the number to a string
-dataframe["Location"] = dataframe["Location"].replace(
-    {1.0: "To visit", 0.0: "Airbnb listing"}
-)
+# listings with a price above 1000 pesos per night
+df = df[df["price"] <= 1000]
 
 # Display dataframe and text
 st.dataframe(dataframe)
-st.markdown("Below is a map showing all the Airbnb listings with a red dot and the location we've chosen with a blue dot.")
+st.markdown("Below is a map showing all the Airbnb listings with price less than 1000 pesos")
 
 # Create the plotly express figure
 fig = px.scatter_mapbox(
-    dataframe,
-    lat="Latitude",
-    lon="Longitude",
-    color="Location",
+    df,
+    lat="latitude",
+    lon="longitude",
+    color="price",
+    size="price",
+    size_max=10, 
+    color_continuous_scale=px.colors.sequential.Reds, 
     zoom=11,
-    height=500,
-    width=800,
-    hover_name="Price",
+    height=800,
+    width=950,
+    hover_name="price",
     hover_data=["Meters from chosen location", "Location"],
-    labels={"color": "Locations"},
+    labels={'price':'Price'},
 )
-fig.update_geos(center=dict(lat=dataframe.iloc[0][2], lon=dataframe.iloc[0][3]))
-fig.update_layout(mapbox_style="stamen-terrain")
 
-# Show the figure
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+fig.update_layout(
+    mapbox_style='dark',
+    autosize=True,
+    hovermode='closest',
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        bearing=0
+    ),
+)
+
+fig.update_geos(center=dict(lat=df.iloc[0][2], lon=df.iloc[0][3]))
+
+Show the figure
 st.plotly_chart(fig, use_container_width=True)
